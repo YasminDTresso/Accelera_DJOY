0b1ad44264285e96568269e781ca994cd4517bde
Yasmin0909 <ydt060709@gmail.com>
2024-12-01 02:31:41 -0300

    Implementando tabela

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,18 +38,6 @@ multi_link.addEventListener("click", function(){
 })
 
 
-/*---------------+DATATABLE+---------------*/
-$(document).ready(function(){
-
-    var table = $('#tabela').DataTable({
-
-        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
-    });
-
-    table.buttons.container().appendTo('#example_wrapper .col-md-6:eq(0)');
-
-}
-)
 
 
 
